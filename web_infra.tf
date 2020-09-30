99feefe67685af7936539a8ed6f63206248259b0
JaeyoungHeo <jay.jaeyoung@gmail.com>
2020-09-30 16:13:20 +0900

    ADD: web ec2 instance

--- web_infra.tf
+++ web_infra.tf
@@ -1,3 +1,7 @@
+data "aws_security_group" "default" {
+  id = "sg-0bfb25745a0e5fc19"
+}
+
 resource "aws_key_pair" "web_admin" {
   key_name = "web_admin"
   public_key = file("~/.ssh/id_rsa.pub")
@@ -12,4 +16,14 @@ resource "aws_security_group" "ssh" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-03b42693dc6a7dc35"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.web_admin.key_name
+  vpc_security_group_ids = [
+    aws_security_group.ssh.id,
+    data.aws_security_group.default.id
+  ]
 }
